4275da3cc7c09fbe5b2d4d483a90dbd39710c2d0
Gabe Arenas <garenas@unomaha.edu>
2024-11-21 16:00:04 -0600

    Revise items.

--- src/homeinventory/common.py
+++ src/homeinventory/common.py
@@ -2,7 +2,18 @@ from dataclasses import dataclass
 
 
 @dataclass
-class Item:
+class InventoryItem:
     """Attributes to describe an item."""
     itemid: int
     name: str
+
+
+@dataclass
+class InventoryItemGroup:
+    """Metadata for a items in a box."""
+    pass
+
+
+class InventoryItemBox:
+    """A collection of items stored in a box."""
+    pass
